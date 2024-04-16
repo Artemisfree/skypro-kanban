8d4b21ed38aacec565ca70ae0d5ffc74005e0af2
Artem <nadtocheev.nadtocheev@gmail.com>
2024-04-16 14:30:31 +0300

    fixed cardsData list

---
+++ src/components/Main/cardsData.js
@@ -0,0 +1,71 @@
+export const cardsData = [
+	{
+		id: 1,
+		theme: 'orange',
+		title: 'Web Design',
+		date: '30.10.23',
+		content: 'Название задачи',
+	},
+	{
+		id: 2,
+		theme: 'green',
+		title: 'Research',
+		date: '30.10.23',
+		content: 'Название задачи',
+	},
+	{
+		id: 3,
+		theme: 'orange',
+		title: 'Web Design',
+		date: '30.10.23',
+		content: 'Название задачи',
+	},
+	{
+		id: 4,
+		theme: 'purple',
+		title: 'Copywriting',
+		date: '30.10.23',
+		content: 'Название задачи',
+	},
+	{
+		id: 5,
+		theme: 'orange',
+		title: 'Web Design',
+		date: '30.10.23',
+		content: 'Название задачи',
+	},
+]
+
+export const cardsData1 = [
+	{
+		id: 1,
+		theme: 'green',
+		title: 'Research',
+		date: '30.10.23',
+		content: 'Название задачи',
+	},
+]
+
+export const cardsData2 = [
+	{
+		id: 1,
+		theme: 'green',
+		title: 'Research',
+		date: '30.10.23',
+		content: 'Название задачи',
+	},
+	{
+		id: 2,
+		theme: 'purple',
+		title: 'Copywriting',
+		date: '30.10.23',
+		content: 'Название задачи',
+	},
+	{
+		id: 5,
+		theme: 'orange',
+		title: 'Web Design',
+		date: '30.10.23',
+		content: 'Название задачи',
+	},
+]
